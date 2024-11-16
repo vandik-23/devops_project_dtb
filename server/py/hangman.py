f5843542d5c52e7f5fdd2dc8ea60d5286ecf5681
timzoller <tim.zoller@stud.hslu.ch>
2024-11-16 15:01:34 +0100

    inserted function set_state

--- server/py/hangman.py
+++ server/py/hangman.py
@@ -37,7 +37,7 @@ class Hangman(Game):
 
     def set_state(self, state: HangmanGameState) -> None:
         """ Get the complete, unmasked game state """
-        pass
+        self.state = state
 
     def print_state(self) -> None:
         """ Print the current game state """
